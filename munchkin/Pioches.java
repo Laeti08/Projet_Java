3a96ff2403051ad69a5829dcf1329db6ae2dc528
Damien <damien.baurens@student.yncrea.fr>
2020-10-22 12:23:31 +0200

    petit changement sur pioches

--- munchkin/Pioches.java
+++ munchkin/Pioches.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 public class Pioches {
 
     //attributs
-    protected ArrayList <Carte> piocheTresors=new ArrayList <Carte>;
-    protected ArrayList <Carte> piochePorte=new ArrayList <Carte>;
-    protected ArrayList <Carte> defausseTresors=new ArrayList <Carte>;
-    protected ArrayList <Carte> defaussePorte=new ArrayList <Carte>;
+    protected ArrayList<Carte> piocheTresors = new ArrayList<Carte>();
+    protected ArrayList<Carte> piochePorte=new ArrayList <Carte>();
+    protected ArrayList<Carte> defausseTresors=new ArrayList <Carte>();
+    protected ArrayList<Carte> defaussePorte=new ArrayList <Carte>();
 
     //constructeur
-    public Pioches(ArrayList pTresors,ArrayList pPorte, ArrayList dTresors, ArrayList dPorte){
+    public Pioches(ArrayList<Carte> pTresors,ArrayList<Carte> pPorte, ArrayList<Carte> dTresors, ArrayList<Carte> dPorte){
         piocheTresors=pTresors;
         piochePorte=pPorte;
         defausseTresors=dTresors;
