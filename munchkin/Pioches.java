97425c668e658fd980b66d836dca55af64c990ce
Laetitia <laetitia.husson@student.yncrea.fr>
2020-10-22 12:25:46 +0200

    reglage conflit

--- munchkin/Pioches.java
+++ munchkin/Pioches.java
@@ -5,17 +5,10 @@ import java.util.ArrayList;
 public class Pioches {
 
     //attributs
-<<<<<<< Updated upstream
     protected ArrayList<Carte> piocheTresors = new ArrayList<Carte>();
     protected ArrayList<Carte> piochePorte=new ArrayList <Carte>();
     protected ArrayList<Carte> defausseTresors=new ArrayList <Carte>();
     protected ArrayList<Carte> defaussePorte=new ArrayList <Carte>();
-=======
-    protected ArrayList <Carte> piocheTresors=new ArrayList <Carte> ();
-    protected ArrayList <Carte> piochePorte=new ArrayList <Carte>();
-    protected ArrayList <Carte> defausseTresors=new ArrayList <Carte>();
-    protected ArrayList <Carte> defaussePorte=new ArrayList <Carte>();
->>>>>>> Stashed changes
 
     //constructeur
     public Pioches(ArrayList<Carte> pTresors,ArrayList<Carte> pPorte, ArrayList<Carte> dTresors, ArrayList<Carte> dPorte){
