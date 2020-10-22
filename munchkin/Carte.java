bf381c580075b3546649266237e4c67f978d56e7
Damien <damien.baurens@student.yncrea.fr>
2020-10-22 11:51:14 +0200

    Monstre

--- munchkin/Carte.java
+++ munchkin/Carte.java
@@ -4,12 +4,21 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class Carte {
+    // attributs
+    protected String name;
+    protected Map<String,Integer> attributeMod;
+    protected String slot;
+    protected String playPhase;
+    protected int goldValue;
 
-    protected String nom;
-    protected Map modifAttribut;
-    
+    // constructeur
 
-
-
-    
+    public Carte(String nom, Map<String,Integer> modifsAttributs, String emplacement, String phaseJeu, int valeurOr){
+        name = nom;
+        attributeMod = modifsAttributs;
+        slot = emplacement;
+        playPhase = phaseJeu;
+        goldValue = valeurOr;
+    }
+ 
 }
--- munchkin/Monstre.java
+++ munchkin/Monstre.java
@@ -1,5 +1,22 @@
 package munchkin;
 
+import java.util.Map;
+
 public class Monstre extends Carte {
+    
+    int combatForce;
+    int treasures;
+    int levels;
+    Map<String,Integer> preFleeEffect;
+    Map<String,Integer> postFleeEffect;
+
+    public Monstre(String nom, Map<String,Integer> modifsAttributs, String emplacement, String phaseJeu, int valeurOr, int forceDeCombat, int nombreTresors, int nombreNiveaux, Map<String,Integer> effetPreFuite, Map<String,Integer> effetPostFuite) {
+        super(nom, modifsAttributs, emplacement, phaseJeu, valeurOr);
+        combatForce = forceDeCombat;
+        treasures = nombreTresors;
+        levels = nombreNiveaux;
+        preFleeEffect = effetPreFuite;
+        postFleeEffect = effetPostFuite;        
+    }
 
 }
