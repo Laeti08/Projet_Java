2f0b139aec49c0c2a480c0e2146c02141df15266
Laetitia <laetitia.husson@student.yncrea.fr>
2020-10-22 10:38:01 +0200

    modif joueur +classe hand

---
+++ munchkin/Hand.java
@@ -0,0 +1,24 @@
+package jeu;
+
+public class Hand {
+    //attribut
+    private int nombreCartes;
+    private ArrayListe<Carte> listeCarte=new ArrayListe<Carte>;
+
+
+    //constructeur
+    public Hand(int numberCard, ArrayListe listCard){
+        nombreCartes=numberCard;
+        listeCarte=listCard;
+    }
+
+    //methodes
+
+    public getCard(){
+
+    }
+    
+    public dropCard(){
+        
+    }
+}
