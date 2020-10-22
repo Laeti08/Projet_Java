3e55e29c138a87bd9addc056898bc2da1d9736d8
Laetitia <laetitia.husson@student.yncrea.fr>
2020-10-22 12:03:34 +0200

    classe race modif

--- munchkin/Race.java
+++ munchkin/Race.java
@@ -1,5 +1,36 @@
 package munchkin;
 
+import java.util.Map;
+
 public class Race extends Carte {
-    
+
+
+    //attribut 
+
+    //constructeur
+
+    public Race(String nom, Map<String,Integer> modifsAttributs, String emplacement, String phaseJeu, int valeurOr, int forceDeCombat, int nombreTresors, int nombreNiveaux, Map<String,Integer> effetPreFuite, Map<String,Integer> effetPostFuite) {
+        super(nom, modifsAttributs, emplacement, phaseJeu, valeurOr);
+            
+    }
+
+    //methode
+
+    public getNain(){
+
+    }
+
+    public getElfe(){
+
+
+    }
+
+    public getHalfelin(){
+
+
+    }
+   
+
 }
+  
+
