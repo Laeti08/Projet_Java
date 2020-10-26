74b9957245f4fbe55e9121e28fe36d53130bb8e7
Laetitia <laetitia.husson@student.yncrea.fr>
2020-10-26 18:01:22 +0100

    Ajouter/retirer carte main du joueur

--- munchkin/Hand.java
+++ munchkin/Hand.java
@@ -16,11 +16,27 @@ public class Hand {
 
     //methodes
 
-    public void getCard(){
-
+    //Ajout le nombre de carte choisi à la main du joueur
+    public void getCard(Carte card, int nombreCartePiocher){
+        nombreCartes=nombreCartes+nombreCartePiocher;
+        listeCarte.add(card);
     }
     
-    public void dropCard(){
+
+    //Retire le nombre de carte choisi de la main du joueur
+    public void dropCard(Carte card, int nombreCarteRetirer){
+        nombreCartes=nombreCartes+nombreCarteRetirer;
+        listeCarte.remove(card);
+    }
+
+    //Verifie le nombre de carte presente dans la main du joueur
+    public void verifCard(){
+        int deleteCard=0;
+        if (numberCard.size()>5){
+            deleteCard=numberCard.size()-5;
+            dropCard(card,deleteCard);
+
+        }
 
     }
 }
